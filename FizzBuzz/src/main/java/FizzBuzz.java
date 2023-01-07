d705f06ceaf8a559f56c9699d402a25aafbe4f51
tcjiao <zeusjiaotianci@gmail.com>
2023-01-07 22:22:20 +0000

    Saved progress on FizzBuzz 2023-01-07 22:22:20 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,3 +1,4 @@
+import javax.lang.model.util.ElementScanner6;
 
 public class FizzBuzz {
     /**
@@ -15,6 +16,17 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
+        if (n % 3 == 0){
+            if (n % 5 == 0){
+                return "FizzBuzz";
+            }
+            else
+            return "Fizz";
+        }
+        else if (n % 5 == 0){
+            return "Buzz";
+        }
+        else
         return "";
     }
 }
